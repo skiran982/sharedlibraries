a9388fba3f34045356f1aca5a7f88eafd9c6e58d
skiran982 <131404130+skiran982@users.noreply.github.com>
2023-05-02 12:15:52 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def newBuild()
 {
    sh 'mvn package'
 }
+
+def newDeployment()
+{
+   deploy adapters: [tomcat9(credentialsId: '477ac95a-01e5-4db3-b6ed-beac3a63d586', path: '', url: 'http://172.31.41.160:8080')], contextPath: 'testapp1', war: '**/*.war'
+}
